4a0d33bf90a4443722d8b21d0f415f36d41e53a0
somsom <som.jaiswal.sj@gmail.com>
2020-10-08 20:50:21 +0530

    PUSHING REACT APP

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://somj57.pythonanywhere.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Somya Jaiswal
         </a>
       </header>
     </div>
