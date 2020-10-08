cd28e0d4bd34680664d599d96df3580ea997f5a4
somsom <som.jaiswal.sj@gmail.com>
2020-10-08 20:49:42 +0530

    Updates

---
+++ precache-manifest.59ff194fb20cd7bd8db880f067b0135d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afca55391db1823d0fdf57f576a07a57",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "202da99a7ac598cf3f31",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea5cfc3202a04704897a",
+    "url": "/react-gh-pages/static/js/2.a51ad747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.a51ad747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202da99a7ac598cf3f31",
+    "url": "/react-gh-pages/static/js/main.1ed0a389.chunk.js"
+  },
+  {
+    "revision": "66e191b5fd3a61b4dd4a",
+    "url": "/react-gh-pages/static/js/runtime-main.732442c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
